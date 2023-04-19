b1ee30a5d0e14fe743c1cb9f48141f85fcfd1f46
PHCaesar <philipp.cserich@gmail.com>
2023-04-19 17:26:40 +0200

    initial commit

---
+++ sj23_TSC_Ost/src/main/java/at/tsc/sj23_tsc_ost/Sj23TscOstApplication.java
@@ -0,0 +1,13 @@
+package at.tsc.sj23_tsc_ost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sj23TscOstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sj23TscOstApplication.class, args);
+	}
+
+}
---
+++ sj23_TSC_Ost/src/test/java/at/tsc/sj23_tsc_ost/Sj23TscOstApplicationTests.java
@@ -0,0 +1,13 @@
+package at.tsc.sj23_tsc_ost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sj23TscOstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
