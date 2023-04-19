219c6461e6b7c465ab9eb5848eef9065fea1f72b
nicolas safarik <nicolas.safarik@gmail.com>
2023-04-19 18:46:29 +0200

    Sports & Sportstype

---
+++ sj23_TSC_Ost/src/main/java/at/tsc/sj23_tsc_ost/domain/Sports.java
@@ -0,0 +1,45 @@
+package at.tsc.sj23_tsc_ost.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.jpa.domain.AbstractPersistable;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "sports")
+public class Sports extends AbstractPersistable<Long> {
+
+    @jakarta.persistence.Version
+    private Integer Version;
+
+    @CreationTimestamp
+    private LocalDateTime creationTimeStamp;
+
+    @UpdateTimestamp
+    private LocalDateTime updateTimeStamp;
+
+    @NotEmpty
+    @NotBlank
+    @Column(name = "sports_name")
+    private String name;
+
+    @NotEmpty
+    @NotBlank
+    @Column(name = "sports_description")
+    private String description;
+
+    @Enumerated
+    @NotNull
+    private SportsType sportsType;
+
+}
---
+++ sj23_TSC_Ost/src/main/java/at/tsc/sj23_tsc_ost/domain/SportsType.java
@@ -0,0 +1,4 @@
+package at.tsc.sj23_tsc_ost.domain;
+public enum SportsType {
+    WINTER, SUMMER, WATER, CONTACT
+}
